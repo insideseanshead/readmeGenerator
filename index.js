49610748d2844be6343c7469b5ff078523b5c92b
Sean Morgan <insideseanshead@gmail.com>
2020-09-30 20:52:49 -0700

    complete questions array

--- index.js
+++ index.js
@@ -13,6 +13,47 @@ const questions = [
         type: 'input',
         message: 'What is your READMEs title?',
         name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Descripe your application.',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Provice installation instructions.',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'What are the uses for your application?',
+        name: "usage"
+    },
+    {
+        type: 'list',
+        message: 'Choose a license for your application.',
+        name: 'license',
+        choices: []
+    },
+    {
+        type: 'input',
+        message: 'Who contributed to this application?',
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        message: 'test?',
+        name: 'tests'
+    },
+    {
+        type: 'input',
+        message: 'What is your github user name?',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email'
     }
 ]
 // TODO: write a readme in a markdown file as a template
